ce26ddde2e31673ffdcfa7716a676707b6f3b873
salem-karim <salem.karim@icloud.com>
2024-10-13 17:24:48 +0200

    Server works horay onto handling requests

--- src/main/java/org/mtcg/Main.java
+++ src/main/java/org/mtcg/Main.java
@@ -1,21 +1,9 @@
 package org.mtcg;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
-
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      System.out.println("i = " + i);
-    }
-  }
-
-  public Main() {
-    System.out.println("Main");
+    System.out.println("Hello and welcome!");
+    var Http = new HttpServer(10001);
+    Http.run();
   }
 }
