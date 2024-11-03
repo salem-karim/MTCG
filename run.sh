185754ac8073f588ccb4a25c92cceeea627d4668
salem-karim <salem.karim@icloud.com>
2024-11-03 16:45:00 +0100

    remove build section of pom.xml and add compile command to run sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 docker build -t mctg_postgres .
 docker run -d -p 5432:5432 --name mtcgdb mctg_postgres
+mvn clean compile
 mvn exec:java -Dexec.mainClass=org.mtcg.Main
 docker stop mtcgdb
 docker rm mtcgdb
